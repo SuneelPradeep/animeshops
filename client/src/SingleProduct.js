c3ee11b1767db4e0cccd14aad5a5838cda6ead65
SuneelPradeep <ernestsolomoninzion@gmail.com>
2024-08-03 14:50:02 +0530

    animeshops

--- client/src/SingleProduct.js
+++ client/src/SingleProduct.js
@@ -34,6 +34,7 @@ const SingleProduct= ()=>{
         getSingleProduct(`${API}?_id=${ids}`);
     }
     hasRun.current= true;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
 }, []);
  
    if(isSingleLoading){
