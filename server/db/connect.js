68ca4296a6f76888faaa6d278cac31107266e2c9
SuneelPradeep <ernestsolomoninzion@gmail.com>
2024-08-04 19:33:14 +0530

    animeshops

--- server/db/connect.js
+++ server/db/connect.js
@@ -4,11 +4,8 @@ const mongoose = require('mongoose')
 // "mongodb+srv://Suneel:SuperSolomon@mydata.jh422jz.mongodb.net/?retryWrites=true&w=majority"
 
 const connectDB = ()=>{
-   // ////console.log('connect DB')
-    return mongoose.connect(process.env.MONGODB_URL,{
-        useNewUrlParser : true,
-        useUnifiedTopology: true, 
-    })
+   console.log('reached here,connect DB',process.env.MONGODB_URL)
+    return mongoose.connect(process.env.MONGODB_URL)
 }
 
 
---
+++ server/netlify/functions/products.js
@@ -0,0 +1,69 @@
+
+const connectDB = require('../../db/connect');
+const { getAllProducts } = require('../../controllers/products'); 
+
+const ProductsModel = require('../../models/product'); 
+
+exports.handler = async (event, context) => {
+    try {
+        await connectDB();
+        console.log('DB connected in serverless');
+
+        // Parse query parameters from event
+        const query = event.queryStringParameters || {}; 
+        const { _id, company, name, featured, sort, select, page, limit } = query;
+
+        const reqObject = {};
+
+        // Build query object
+        if (company) {
+            reqObject.company = { $regex: company, $options: "i" };
+        }
+        if (name) {
+            reqObject.name = { $regex: name, $options: "i" };
+        }
+        if (featured) {
+            reqObject.featured = featured === 'true';
+        }
+
+        let apiData = ProductsModel.find(reqObject);
+
+        // Sort
+        if (sort) {
+            const sortBy = sort.replace(",", " ");
+            apiData = apiData.sort(sortBy);
+        }
+
+        // Select fields
+        if (select) {
+            const selectFields = select.split(",").join(" ");
+            apiData = apiData.select(selectFields);
+        }
+
+        // Pagination
+        const pageNumber = Number(page) || 1;
+        const limitNumber = Number(limit) || 10;
+        const skip = (pageNumber - 1) * limitNumber;
+
+        apiData = apiData.skip(skip).limit(limitNumber);
+
+        // Fetch data
+        let products;
+        if (_id) {
+            products = await ProductsModel.findById(_id);
+        } else {
+            products = await apiData;
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(products),
+        };
+    } catch (error) {
+        console.error('Database connection error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to connect to database' }),
+        };
+    }
+};
